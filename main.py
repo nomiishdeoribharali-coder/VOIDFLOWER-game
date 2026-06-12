950031217c80d883404d4b2bab65c96d85d5d108
nomiishdeoribharali-coder <nomiish.deori.bharali@gmail.com>
2026-06-12 19:22:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,12 @@ from engine.core import Game
 from game.menus import TitleScene, GameOverScene
 from game.main import GameScene
 from game.constants import WINDOW_WIDTH, WINDOW_HEIGHT, FPS
-
-
+# i woke up in a new buggati
 def main():
     game = Game(title="Voidflower", width=WINDOW_WIDTH, height=WINDOW_HEIGHT, fps=FPS)
-
     game.add_scene("title", TitleScene(game))
     game.add_scene("game", GameScene(game))
     game.add_scene("game_over", GameOverScene(game))
-
     game.run(start_scene="title")
-
-
 if __name__ == "__main__":
     main()
